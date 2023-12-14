95d74d261ec700c354713526f2b60d3890ef3209
曹绪培 <2877406366@qq.com>
2023-12-14 21:52:38 +0800

    maven打包配置

--- kdg-service/src/main/java/com/kdg/refactoring/kdgservice/travel/controller/GhDayInfoController.java
+++ kdg-service/src/main/java/com/kdg/refactoring/kdgservice/travel/controller/GhDayInfoController.java
@@ -8,7 +8,6 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.io.Serializable;
 import java.util.List;
 
 /**
